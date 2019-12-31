e11a7c034bcf2e7565b8e7d2b5f47c6a263ac98b
Gildo Junior <1gildojunior@gmail.com>
2019-12-31 19:21:50 -0300

    Inicio da implementacao da tela de exibicai do tabuleiro

--- XadrezConsole/tabuleiro/Tabuleiro.cs
+++ XadrezConsole/tabuleiro/Tabuleiro.cs
@@ -2,12 +2,16 @@
     class Tabuleiro {
         public int Linhas { get; set; }
         public int Colunas { get; set; }
-        public Peca[,] pecas;
+        private Peca[,] pecas;
 
         public Tabuleiro(int linhas, int colunas) {
             Linhas = linhas;
             Colunas = colunas;
             pecas = new Peca[Linhas, Colunas];
+        }
+
+        public Peca peca(int linha, int coluna) {
+            return pecas[linha, coluna];
         }
     }
 }
