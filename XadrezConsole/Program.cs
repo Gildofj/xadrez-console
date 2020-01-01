ff12025cad9e6f50132b246091d3ac3b365e7558
Gildo Junior <1gildojunior@gmail.com>
2020-01-01 10:34:35 -0300

    Melhorias na implementacao imprimir tabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -10,7 +10,9 @@ namespace XadrezConsole {
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(3, 6));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
                 Tela.imprimirTabuleiro(tab);
             } 
